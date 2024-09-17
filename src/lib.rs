9bfe4310f9204c8e6c7853d071b576f09d94e5d5
Stéphane Mourey <mail@stephanemourey.fr>
2024-09-17 14:49:16 +0200

    Add skeleton

---
+++ src/lib.rs
@@ -0,0 +1,24 @@
+pub fn reverse(s: &str) -> String {
+  "".to_string()
+}
+
+pub fn is_palindrome(s: &str) -> String {
+  "".to_string()
+}
+
+pub fn remove_vowels(s: &str) -> String {
+  "".to_string()
+}
+
+pub fn rle_encode(s: &str) -> String {
+  "".to_string()
+}
+pub enum txtToCase {
+  SnakeCase,
+  CamelCase,
+  KebabCase,
+}
+
+pub fn to_case(s: &str, c: txtToCase) -> String {
+  "".to_string()
+}
