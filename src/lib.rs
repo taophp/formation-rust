88e41e13e11fbbc6ca354fd116aab564aac85d62
Stéphane Mourey <mail@stephanemourey.fr>
2024-11-06 17:42:17 +0100

    Réécriture de l'étape test pour permettre la compilation

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,67 @@
 ///     * `state` which identify the version A or B with an ulid,
 ///     * and `history` which stores successiv states in a vector.
 /// It is the responsability of the application to store and provide those Mogulid beside things to handle. The application must provide a Mogulid for each version.
+
+use ulid::Ulid;
+use serde::{Serialize, Deserialize};
+
+#[derive(Debug, PartialEq, Serialize)]
+pub enum MogulidError {
+    NotVersionsOfTheSameObject,
+    ChallengerOlderThanOriginal,
+    IdenticalVersions,
+    Conflict,
+}
+
+#[derive(Debug, Eq, PartialEq, Serialize, Deserialize)]
+pub struct Mogulid {
+    ulid: Ulid,
+    state: Ulid,
+    history: Vec<Ulid>
+}
+
+#[allow(dead_code)]
+impl Mogulid {
+    pub fn new(ulid: Ulid, state: Ulid, history: Vec<Ulid>) -> Self {
+        Mogulid {
+            ulid,
+            state,
+            history
+        }
+    }
+
+    pub fn new_start() -> Self {
+        Mogulid {
+            ulid: Ulid::new(),
+            state: Ulid::new(),
+            history: vec![]
+        }
+    }
+
+    pub fn define_new_state(&mut self) {
+    }
+
+    pub fn get_ulid(&self) -> Ulid {
+        self.ulid
+    }
+
+    pub fn get_state(&self) -> Ulid {
+        self.state
+    }
+
+    pub fn get_history(&self) -> Vec<Ulid> {
+        self.history.clone()
+    }
+}
+
+/// ## Allowing merge of versions
+///
+/// You can use this function to know if two versions can be merge and, if not, why.
 ///
 /// ```
 /// use mogulid::{Mogulid, MogulidError, mogulid_allow_update};
 /// use ulid::Ulid;
 ///
-/// // Exemple d'initialisation d'un `Mogulid`.
 /// let original = Mogulid::new(Ulid::new(), Ulid::new(), vec![Ulid::new(), Ulid::new()]);
 /// let challenger_not_same = Mogulid::new(Ulid::new(), Ulid::new(), vec![Ulid::new(), Ulid::new()]);
 ///
@@ -37,7 +92,11 @@
 /// challenger_allowed.define_new_state();
 /// assert_eq!(mogulid_allow_update(&original, &challenger_allowed), Ok(()));
 /// ```
-///
+#[allow(dead_code)]
+pub fn mogulid_allow_update(_original:&Mogulid, _challenger:&Mogulid) -> Result<(), MogulidError> {
+  Ok(())
+}
+
 /// ## Merging versions
 ///
 /// When two versions are valid and must be merged, we want to merge their histories to get a new Mogulid
@@ -53,16 +112,19 @@
 /// let merged = mogulid_merge(&original, &challenger_allowed);
 /// assert_eq!(merged.get_history().len(), original.get_history().len() + 1);
 /// ```
+pub fn mogulid_merge(_original:&Mogulid, _challenger:&Mogulid) -> Mogulid {
+    Mogulid::new(Ulid::new(), Ulid::new(), vec![])
+}
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
         use ulid::Ulid;
-        use crate::{MogulidError, Mogulid, mogul_allow_update};
+        use crate::{MogulidError, Mogulid, mogulid_allow_update};
 
         let original = Mogulid { ulid: Ulid::new(), state: Ulid::new(), history: vec![Ulid::new(),Ulid::new()]};
         let challenger_not_same = Mogulid { ulid: Ulid::new(), state: Ulid::new(), history: vec![Ulid::new(),Ulid::new()]};
-        assert_eq!(mogul_allow_update(&original, &challenger_not_same), Err(MogulidError::NotVersionsOfTheSameObject));
+        assert_eq!(mogulid_allow_update(&original, &challenger_not_same), Err(MogulidError::NotVersionsOfTheSameObject));
     }
 }
